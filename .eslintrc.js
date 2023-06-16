26b3a9d6f42452650b071fb775fc9c0a49a6e7e9
Manuelortegaj11 <manuelortegajuvinao@gmail.com>
2023-06-16 15:11:34 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prefer-template': 0,
+    'react/no-children-prop': [0],
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    "react/destructuring-assignment": "off"
+  },
+};
